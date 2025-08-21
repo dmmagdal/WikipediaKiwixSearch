117dff27bf19d70d367c8904c9e796bec8b7f045
Diego Magdaleno <magdaleno_m@sbcglobal.net>
2025-08-21 14:37:38 -0400

    Validated question generation across different models.

--- generate_questions.py
+++ generate_questions.py
@@ -12,12 +12,12 @@ import os
 import random
 import shutil
 
-from libzim.reader import Archive
 import numpy as np
 import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from transformers import pipeline
+from transformers import AutoModelForCausalLM
+from transformers import pipeline, set_seed
 
 from searchwiki import get_all_articles, get_article_entries
 from searchwiki import get_article_lengths, get_random_paragraph_from_article
@@ -27,6 +27,7 @@ from searchwiki import get_article_lengths, get_random_paragraph_from_article
 seed = 1234
 random.seed(seed)
 np.random.seed(seed)
+set_seed(seed)
 
 
 def main():
@@ -78,7 +79,12 @@ def main():
 	# GENERATE QUESTION.
 	###################################################################
 	# Model IDs & their respective folders.
-	models = ["google/flan-t5-large", "facebook/bart-large"]
+	models = [
+		"google/flan-t5-large", "facebook/bart-large",
+		"TinyLlama/TinyLlama-1.1B-Chat-v1.0", 
+		# "meta-llama/Llama-3.2-1B-Instruct",
+		# "meta-llama/Llama-3.2-1B",
+	]
 	model_paths = [model_id.replace("/", "_") for model_id in models]
 	model_caches = [model_path + "_tmp" for model_path in model_paths]
 
@@ -104,18 +110,43 @@ def main():
 	for m_idx, model_id in enumerate(models):
 		print(f"Running model: {model_id}")
 
+		if "meta-llama" in model_id:
+			if not os.path.exists(".env"):
+				print("Error: Requre valid huggingface token stored in .env file.")
+				print("File not found")
+				exit(1)
+
+			with open(".env", "r") as f:
+				token = f.read().strip()
+		else:
+			token = None
+
+		# Get model pathing.
 		model_dir = model_paths[m_idx]
 		model_cache = model_caches[m_idx]
+
+		# Whether a llama model is being used.
+		llama_model = "Llama" in model_id
+
+		# Download the model and tokenizer if needed.
 		if not os.path.exists(model_dir):
-			# Download the model and tokenizer.
 			tokenizer = AutoTokenizer.from_pretrained(
 				model_id, 
-				cache_dir=model_cache
+				cache_dir=model_cache,
+				token=token,
+				use_fast=True,
 			)
-			model = AutoModelForSeq2SeqLM.from_pretrained(
-				model_id,
-				cache_dir=model_cache
-			)
+			if llama_model:
+				model = AutoModelForCausalLM.from_pretrained(
+					model_id,
+					cache_dir=model_cache,
+					token=token,
+				)
+			else:
+				model = AutoModelForSeq2SeqLM.from_pretrained(
+					model_id,
+					cache_dir=model_cache,
+				)
 
 			# Save the model and tokenizer.
 			tokenizer.save_pretrained(model_dir)
@@ -126,7 +157,10 @@ def main():
 		
 		# Load the model and tokenizer.
 		tokenizer = AutoTokenizer.from_pretrained(model_dir)
-		model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
+		if llama_model:
+			model = AutoModelForCausalLM.from_pretrained(model_dir)
+		else:
+			model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
 
 		# Iterate through the passages.
 		for idx, passage in tqdm(enumerate(query_passages)):
@@ -143,20 +177,27 @@ def main():
 
 			# Generate the question by passing the prompt to the model
 			# pipeline.
+			task = "text-generation" if llama_model else "text2text-generation"
 			model_pipe = pipeline(
-				"text2text-generation", 
+				task, 
 				model=model,
 				tokenizer=tokenizer, 
 				device=device
 			)
 			model_output = model_pipe(
 				prompt, 
-				max_length=512, 
+				# max_length=512,
+				max_length=tokenizer.model_max_length, 
+				# do_sample=llama_model,
+				# temperature=0.7 if llama_model else 1.0,
 				# num_return_sequences=5	# Cannot use this if using greedy decoding (default params).
 			)
 
 			# Update the output.
 			output[idx].update({model_id: model_output})
+	
+		# Print statement for spacing the console.
+		print()
 
 	# Save the output to JSON.
 	with open("questions.json", "w+") as f:
@@ -164,8 +205,15 @@ def main():
 
 	# NOTE:
 	# It seems like Flan-T5 had the best generated questions. 
-	# BART-large seemed to include the initial prompt and appened a 
+	# - BART-large: Appears to include the initial prompt and appends a
 	# random segment of the input passage/context after that.
+	# - TinyLlama-1.1B-Chat-v1.0: Either rambles or repeats parts of 
+	# the input passage/context. This includes even when altering the
+	# sampling and temperature parameters.
+	# - Llama-3.2-1B-Instruct (and Llama-3.2-1B): Hangs or does not 
+	# generate a response in a timely manner. Could be due to the use 
+	# of the text-generation pipeline or not following the chat prompt 
+	# format that is expected for "chat"/"instruct" models. 
 
 	# Exit the program.
 	exit(0)
